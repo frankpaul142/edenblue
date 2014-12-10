a24b9fd92a3126125c68f1a15686c6025ab178b4
Rodney Ledesma <rodney.ledesma@share.com.ec>
2014-12-10 16:52:28 -0500

    cambio adminemail

--- protected/config/main.php
+++ protected/config/main.php
@@ -83,7 +83,7 @@ return array(
     // using Yii::app()->params['paramName']
     'params' => array(
         // this is used in contact page
-        'adminEmail' => 'webmaster@example.com',
+        'adminEmail' => 'rodney.ledesma@share.com.ec',
     ),
     'behaviors' => array('ApplicationConfigBehavior')
 );
